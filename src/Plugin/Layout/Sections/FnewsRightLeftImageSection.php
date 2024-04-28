651d8a01fbd41ae06978d245df3c53d3f11b1bd5
kouwa stephane <kksasteph888@gmail.com>
2024-04-28 06:21:54 +0200

    Maj

--- src/Plugin/Layout/Sections/FnewsRightLeftImageSection.php
+++ src/Plugin/Layout/Sections/FnewsRightLeftImageSection.php
@@ -5,11 +5,12 @@ namespace Drupal\fast_models\Plugin\Layout\Sections;
 use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 use Drupal\formatage_models\FormatageModelsThemes;
 use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
-
+use Drupal\Core\Form\FormStateInterface;
 
 /**
- * 
- *  FnewsRightLeftImage
+ *
+ * FnewsRightLeftImage
+ *
  * @Layout(
  *  id = "f_news_right_left_image",
  *  label = @Translation("f_news_right_left_image"),
@@ -20,97 +21,136 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *  default_region = "content",
  *  regions = {
  *      "right_left_title" = {
- *          "label" = @Translation("right_left_title"),     
+ *          "label" = @Translation("right_left_title"),
  *      },
  *      "right_left_text" = {
  *          "label" = @Translation("right_left_text"),
  *      },
  *      "right_left_button" = {
- *          "label" = @Translation("right_left_button"),     
+ *          "label" = @Translation("right_left_button"),
  *      },
  *      "right_left_image" = {
  *          "label" = @Translation("right_left_image"),
  *      }
  *  }
  * )
- * 
+ *
  */
-
 class FnewsRightLeftImageSection extends FormatageModelsSection {
-    /**
-     *
-     * {@inheritdoc}
-     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
-     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
-        // TODO Auto-generated method stub
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
-        $this->pluginDefinition->set('icon', $this->pathResolver->getPath('module', 'fast_models') . "/icons/sections/f_news_right_left_image_map.png");
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels:build()
-     * 
-     */
-    public function build(array $regions) {
-
-        // TODO Auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-
-        return $build;
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     * 
-     */
-    public function defaultConfiguration() {
-        return parent::defaultConfiguration() + [
-            'css' => '',
-            'fnrls' => [
-                'builder-form' => true,
-                'region_css_right_left_title' => 'h1',
-                'info' => [
-                    'title' => 'Contenu',
-                    'loader' => 'static'
-                ],
-                'fields' => [
-                    'right_left_title' => [
-                        'text_html' => [
-                            'label' => 'Titre',
-                            'value' => "Name of First Feature"
-                        ]
-                    ],
-                    'right_left_text' => [
-                        'text_html' => [
-                            'label' => 'Paragraphe',
-                            'value' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                        tempor incididunt ut labore."
-                        ]
-                    ],
-                    'right_left_button' => [
-                        'url' => [
-                            'label' => 'Button',
-                            'value' => [
-                                'link' => "#",
-                                'text' => "learn more <i class='btn-angle-right'></i>",
-                                'class' => "load-more-button"
-                            ]
-                        ]
-                    ],
-                    'right_left_image' => [
-                        'text_html' => [
-                            'label' => 'Image',
-                            'value' => "<img src='https://demo.morethanthemes.com/flashyplus-news8/default/sites/default/files/styles/mt_large/public/2019-07/mt-service-2-highlight-1.jpg?itok=Gr5UxBWz'
-                         alt=''>"
-                        ]
-                    ]
-                ]
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
+    // TODO Auto-generated method stub
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+    $this->pluginDefinition->set('icon', $this->pathResolver->getPath('module', 'fast_models') . "/icons/sections/f_news_right_left_image_map.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels:build()
+   *
+   */
+  public function build(array $regions) {
+    
+    // TODO Auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['class_row'] = [
+      '#type' => 'textfield',
+      '#title' => 'Class row',
+      '#default_value' => $this->configuration['class_row']
+    ];
+    $form['class_col_left'] = [
+      '#type' => 'textfield',
+      '#title' => 'Class col left',
+      '#default_value' => $this->configuration['class_col_left']
+    ];
+    $form['class_col_right'] = [
+      '#type' => 'textfield',
+      '#title' => 'Class col right',
+      '#default_value' => $this->configuration['class_col_right']
+    ];
+    return $form;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    $this->configuration['class_row'] = $form_state->getValue('class_row');
+    $this->configuration['class_col_left'] = $form_state->getValue('class_col_left');
+    $this->configuration['class_col_right'] = $form_state->getValue('class_col_right');
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   *
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'css' => 'space-inner-padding',
+      'class_row' => 'row justify-content-between',
+      'class_col_left' => 'col-12 col-md-6',
+      'class_col_right' => 'col-12 col-md-6',
+      'fnrls' => [
+        'builder-form' => true,
+        'region_css_right_left_title' => 'h1',
+        'info' => [
+          'title' => 'Contenu',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'right_left_title' => [
+            'text_html' => [
+              'label' => 'Titre',
+              'value' => "Name of First Feature"
             ]
-        ];
-    }
+          ],
+          'right_left_text' => [
+            'text_html' => [
+              'label' => 'Paragraphe',
+              'value' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore."
+            ]
+          ],
+          'right_left_button' => [
+            'url' => [
+              'label' => 'Button',
+              'value' => [
+                'link' => "#",
+                'text' => "learn more <i class='btn-angle-right'></i>",
+                'class' => "load-more-button"
+              ]
+            ]
+          ],
+          'right_left_image' => [
+            'text_html' => [
+              'label' => 'Image',
+              'value' => "<img src='https://demo.morethanthemes.com/flashyplus-news8/default/sites/default/files/styles/mt_large/public/2019-07/mt-service-2-highlight-1.jpg?itok=Gr5UxBWz'
+                         alt=''>"
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+  
 }
